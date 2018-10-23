9ac7bb024390e4ce96d4cdf62138feb0f3c831b0
Matt <matt.voget@gmail.com>
2018-10-22 20:43:22 -0600

    Using the mongo ORM dll

--- WebConsumer/WebConsumer/Models/Request.cs
+++ WebConsumer/WebConsumer/Models/Request.cs
@@ -1,11 +1,7 @@
-﻿using Newtonsoft.Json;
+﻿using MongoOrm;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
 using System.Runtime.Serialization;
-using System.Web;
 
 namespace WebConsumer.Models
 {
--- WebConsumer/WebConsumer/Repositories/RequestRepository.cs
+++ WebConsumer/WebConsumer/Repositories/RequestRepository.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using MongoOrm;
 using WebConsumer.Models;
 
 namespace WebConsumer.Repositories
