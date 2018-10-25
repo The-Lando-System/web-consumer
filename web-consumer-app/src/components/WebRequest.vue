ed7c002913a39d04de69c6244d6f3e94fcce99eb
Matt <matt.voget@gmail.com>
2018-10-24 22:10:56 -0600

    added initial tabs

--- web-consumer-app/src/components/WebRequest.vue
+++ web-consumer-app/src/components/WebRequest.vue
@@ -2,28 +2,50 @@
 
   <div class="row">
     <div class="col-md-6">
-      <form class="web-request-card">
-        <div class="form-group">
-          <label for="name-input">Name</label>
-          <input v-model="name" type="text" class="form-control" id="name-input" placeholder="Name your request">
+      <div class="web-request-card">
+      
+        <ul class="nav nav-tabs">
+          <li class="nav-item">
+            <a v-on:click="formMode = true" v-bind:class="{'active':formMode}" class="nav-link">New Request</a>
+          </li>
+          <li class="nav-item">
+            <a v-on:click="formMode = false" v-bind:class="{'active':!formMode}" class="nav-link">Saved Requests</a>
+          </li>
+        </ul>
+
+        <div v-if="formMode">
+          <form style="margin-top: 20px;">
+            <div class="form-group">
+              <label for="name-input">Name</label>
+              <input v-model="name" type="text" class="form-control" id="name-input" placeholder="Name your request">
+            </div>
+            <div class="form-group">
+              <label for="url-input">URL</label>
+              <input v-model="url" type="text" class="form-control" id="url-input" placeholder="https://example.com">
+            </div>
+            <div class="form-group">
+              <label for="method-input">Request Method</label>
+              <select v-model="method" id="method-input" class="custom-select">
+                <option value="Get">Get</option>
+                <option value="Post">Post</option>
+              </select>
+            </div>
+            <div class="form-group">
+              <label for="body-input">Body</label>
+              <textarea v-model="postBody" class="form-control" id="body-input" rows="5" placeholder="{some:json}" />
+            </div>
+            <button v-on:click="submit" class="btn btn-primary">Execute</button>
+          </form>
         </div>
-        <div class="form-group">
-          <label for="url-input">URL</label>
-          <input v-model="url" type="text" class="form-control" id="url-input" placeholder="https://example.com">
+        
+        <div v-else>
+          Hello!
         </div>
-        <div class="form-group">
-          <label for="method-input">Request Method</label>
-          <select v-model="method" id="method-input" class="custom-select">
-            <option value="Get">Get</option>
-            <option value="Post">Post</option>
-          </select>
-        </div>
-        <div class="form-group">
-          <label for="body-input">Body</label>
-          <textarea v-model="postBody" class="form-control" id="body-input" rows="5" placeholder="{some:json}" />
-        </div>
-        <button v-on:click="submit" class="btn btn-primary">Execute</button>
-      </form>
+
+      </div>
+
+      
+      
     </div>
 
     <div class="col-md-6">
@@ -46,7 +68,8 @@ export default {
       url: 'http://worldclockapi.com/api/json/est/now',
       method: 'Get',
       postBody: '',
-      response: {}
+      response: {},
+      formMode: true
     }
   },
   methods: {
@@ -92,7 +115,13 @@ export default {
     border: solid 0.5px #e2e1e0;
     border-radius: 2px;
     box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
+    min-height: 400px;
   }
-  .custom-select {
+  .web-request-card .nav-item {
+    cursor: pointer;
   }
+  .active {
+    font-weight: bold;
+  }
+
 </style>
